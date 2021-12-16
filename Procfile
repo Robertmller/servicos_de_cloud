b9f839ccc1c68f6706538bbe7f05c9027c663267
Robert C. da Conceição <80072807+Robertmller@users.noreply.github.com>
2021-12-16 17:44:56 -0300

    Preparando para Deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python3 manage.py migrate
+web: gunicorn servicos_de_cloud.wsgi --preload --log-file -
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'servico_de_cloud.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'servico_de_cloud.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
